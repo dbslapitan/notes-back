a06bdb92ff303ad2d9a5497b7e39c5a1d49ef5c9
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-04 04:45:12 -0500

    add cors and connect to mongodb

--- server.ts
+++ server.ts
@@ -8,10 +8,19 @@ if(env.error){
 
 import express from "express";
 import NormalizePort from "./utils/normalizePort";
+import { connect, connection } from "mongoose";
+import cors from "cors";
 
 const PORT = NormalizePort(process.env.PORT as string);
+const DB_URI = process.env.DB_URI;
 
 const app = express();
+
+connect(DB_URI as string);
+connection.on("connected", () => console.log("Connected to MongoDB..."));
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running at PORT ${PORT}...`);
